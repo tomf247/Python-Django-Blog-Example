f69d4d63b76650f0c396c4c4cd57a56cdb424faa
tomf247 <tomfoley247@gmail.com>
2022-08-29 17:14:06 +0000

    Login and logout

--- codestar/settings.py
+++ codestar/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
@@ -59,6 +63,13 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'codestar.urls'
+
+ACOUNT_EMAIL_VERIFICATION = 'none'
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 TEMPLATES = [
     {
